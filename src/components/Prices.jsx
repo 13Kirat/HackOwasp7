71a19f866c4cded837e5bacc4e93bd6b1c969e10
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-05-14 20:54:26 +0530

    prizes

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -7,15 +7,12 @@ function PricingDemo() {
       <div className="relative z-10 mx-auto max-w-5xl px-4 pb-20 pt-10 md:px-8">
         <div className="mb-12 ">
           <h2 className="text-center text-3xl text-zinc-50 font-semibold leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-            Prizes
+            Hacko Winners
           </h2>
-          {/* <p className="text-center text-base text-muted-foreground md:text-lg">
-            Use it for free for yourself, upgrade when your team needs advanced
-            control.
-          </p> */}
         </div>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
           <PricingCard
+            team="Gamma-coders"
             tier="1st Prize"
             prize="15,000rs"
             bestFor="Best for 1-5 users"
@@ -31,6 +28,7 @@ function PricingDemo() {
             ]}
           />
           <PricingCard
+            team="PixgenZ"
             tier="2nd Prize"
             prize="10,000rs"
             bestFor="Best for 5-50 users"
@@ -42,10 +40,11 @@ function PricingDemo() {
               { text: "Goodies", checked: false },
               { text: "Industry Mentors Support", checked: false },
               { text: "Rust Workshop", checked: false },
-              { text: "3x Ledger Nano", checked: false},
+              { text: "3x Ledger Nano", checked: false },
             ]}
           />
           <PricingCard
+            team="CrypticCrew"
             tier="3rd Prize"
             prize="5,000rs"
             bestFor="Best for 50+ users"
@@ -57,12 +56,13 @@ function PricingDemo() {
               { text: "Goodies", checked: false },
               { text: "Industry Mentors Support", checked: false },
               { text: "Rust Workshop", checked: false },
-              { text: "3x Ledger Nano", checked: false},
+              { text: "3x Ledger Nano", checked: false },
             ]}
           />
         </div>
         <div className="grid grid-cols-1 mt-10 gap-6 md:grid-cols-3">
           <PricingCard
+            team="ForcePush"
             tier="Best First Year"
             prize="2,000rs"
             bestFor="Best for 1-5 users"
@@ -78,21 +78,7 @@ function PricingDemo() {
             ]}
           />
           <PricingCard
-            tier="Hardware"
-            prize="2,000rs"
-            bestFor="Best for 5-50 users"
-            CTA="14-day free trial"
-            benefits={[
-              { text: "TIM HORTONS Coupons 20% OFF", checked: true },
-              { text: "TIM HORTONS Magnets", checked: true },
-              { text: ".xyz Domain", checked: true },
-              { text: "Goodies", checked: false },
-              { text: "Industry Mentors Support", checked: false },
-              { text: "Rust Workshop", checked: false },
-              { text: "3x Ledger Nano", checked: false},
-            ]}
-          />
-          <PricingCard
+            // team="he"
             tier="Web3 Overall"
             prize="85,000rs"
             bestFor="Best for 50+ users"
@@ -106,6 +92,116 @@ function PricingDemo() {
               { text: "Rust Workshop", checked: true },
               { text: "3x Ledger Nano", checked: true },
             ]}
+          />
+          <PricingCard
+            team="CollabIn"
+            tier="Open Innovation"
+            prize="2,000rs"
+            bestFor="Best for 5-50 users"
+            CTA="14-day free trial"
+            benefits={[
+              { text: "TIM HORTONS Coupons 20% OFF", checked: true },
+              { text: "TIM HORTONS Magnets", checked: true },
+              { text: ".xyz Domain", checked: true },
+              { text: "Goodies", checked: false },
+              { text: "Industry Mentors Support", checked: false },
+              { text: "Rust Workshop", checked: false },
+              { text: "3x Ledger Nano", checked: false },
+            ]}
+          />
+        </div>
+        <h2 className="text-center text-3xl mt-20 text-zinc-50 font-semibold leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+          Ideathon Winners
+        </h2>
+        <div className="grid grid-cols-1 mt-10 gap-6 md:grid-cols-3">
+          <PricingCard
+            team="Un-acrobats"
+            tier="1st Prize"
+          // prize="2,000rs"
+          // benefits={[
+          //   { text: "TIM HORTONS Coupons 20% OFF", checked: true },
+          //   { text: "TIM HORTONS Magnets", checked: true },
+          //   { text: ".xyz Domain", checked: true },
+          //   { text: "Goodies", checked: false },
+          //   { text: "Industry Mentors Support", checked: false },
+          //   { text: "Rust Workshop", checked: false },
+          //   { text: "3x Ledger Nano", checked: false },
+          // ]}
+          />
+          <PricingCard
+            team="cryptoBAPS"
+            tier="2nd Prize"
+          // prize="2,000rs"
+          // benefits={[
+          //   { text: "TIM HORTONS Coupons 20% OFF", checked: true },
+          //   { text: "TIM HORTONS Magnets", checked: true },
+          //   { text: ".xyz Domain", checked: true },
+          //   { text: "Goodies", checked: false },
+          //   { text: "Industry Mentors Support", checked: false },
+          //   { text: "Rust Workshop", checked: false },
+          //   { text: "3x Ledger Nano", checked: false },
+          // ]}
+          />
+          <PricingCard
+            team="ImagineDragons"
+            tier="3rd Prize"
+          // prize="85,000rs"
+          // benefits={[
+          //   { text: "TIM HORTONS Coupons 20% OFF", checked: false },
+          //   { text: "TIM HORTONS Magnets", checked: false },
+          //   { text: ".xyz Domain", checked: false },
+          //   { text: "Goodies", checked: true },
+          //   { text: "Industry Mentors Support", checked: true },
+          //   { text: "Rust Workshop", checked: true },
+          //   { text: "3x Ledger Nano", checked: true },
+          // ]}
+          />
+        </div>
+        <h2 className="text-center text-3xl mt-20 text-zinc-50 font-semibold leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+          ML-Contest Winners
+        </h2>
+        <div className="grid grid-cols-1 mt-10 gap-6 md:grid-cols-3">
+          <PricingCard
+            person="yunsuxiaozi - mikeking20030224@qq.com"
+            tier="1st Prize"
+          // prize="2,000rs"
+          // benefits={[
+          //   { text: "TIM HORTONS Coupons 20% OFF", checked: true },
+          //   { text: "TIM HORTONS Magnets", checked: true },
+          //   { text: ".xyz Domain", checked: true },
+          //   { text: "Goodies", checked: false },
+          //   { text: "Industry Mentors Support", checked: false },
+          //   { text: "Rust Workshop", checked: false },
+          //   { text: "3x Ledger Nano", checked: false },
+          // ]}
+          />
+          <PricingCard
+            person="john - raojohng@gmail.com"
+            tier="2nd Prize"
+          // prize="2,000rs"
+          // benefits={[
+          //   { text: "TIM HORTONS Coupons 20% OFF", checked: true },
+          //   { text: "TIM HORTONS Magnets", checked: true },
+          //   { text: ".xyz Domain", checked: true },
+          //   { text: "Goodies", checked: false },
+          //   { text: "Industry Mentors Support", checked: false },
+          //   { text: "Rust Workshop", checked: false },
+          //   { text: "3x Ledger Nano", checked: false },
+          // ]}
+          />
+          <PricingCard
+            person="baithunter635 - tanishktiwari.btechai2428@bpibs.in"
+            tier="3rd Prize"
+          // prize="85,000rs"
+          // benefits={[
+          //   { text: "TIM HORTONS Coupons 20% OFF", checked: false },
+          //   { text: "TIM HORTONS Magnets", checked: false },
+          //   { text: ".xyz Domain", checked: false },
+          //   { text: "Goodies", checked: true },
+          //   { text: "Industry Mentors Support", checked: true },
+          //   { text: "Rust Workshop", checked: true },
+          //   { text: "3x Ledger Nano", checked: true },
+          // ]}
           />
         </div>
       </div>
--- src/components/ui/dark-gradient-pricing.jsx
+++ src/components/ui/dark-gradient-pricing.jsx
@@ -29,44 +29,40 @@ const Benefit = ({
 export const PricingCard = ({
   tier,
   prize,
-  bestFor,
-  CTA,
   benefits,
-  className
+  className,
+  team, person
 }) => {
   return (
     <motion.div
-      initial={{ filter: "blur(2px)" }}
+      initial={{ filter: "blur(2px)", display: "relative", marginTop: "30px" }}
       whileInView={{ filter: "blur(0px)" }}
       transition={{ duration: 0.5, ease: "easeInOut", delay: 0.25 }}>
+      {team && <div className="text-xl -mt-5 absolute top-0 left-1/2 -translate-x-1/2 z-10 text-white text-center border border-gray-300 rounded-full w-[80%] px-4 py-1">Team : {team}</div>}
       <Card
         className={cn(
           "relative h-full w-full overflow-hidden border",
           "border-zinc-700 bg-gradient-to-br from-zinc-950/50 to-zinc-900/80",
-          "p-3 md:p-6",
+          "p-3 md:p-6 pt-8 md:pt-12",
           className
         )}>
-        <div
-          className="flex flex-col items-center border-b pb-3 md:pb-6 border-zinc-700 ">
-          <span className="mb-3 md:mb-6 inline-block text-zinc-50 ">
+        {(tier || prize) && <div
+          className={`flex flex-col items-center ${benefits ? "border-b pb-3 md:pb-6 border-zinc-700" : ""}`}>
+          {tier && <span className="mb-3 md:mb-6 inline-block text-zinc-50 ">
             {tier}
-          </span>
-          <span className="mb-1.5 md:mb-3 inline-block text-4xl font-medium text-zinc-50">
+          </span>}
+          {person && <span className="mb-3 md:mb-6  inline-block text-zinc-50 ">
+            Person : {person}
+          </span>}
+          {prize && <span className="mb-1.5 md:mb-3 inline-block text-4xl font-medium text-zinc-50">
             {prize}
-          </span>
-          {/* <span
-            className="bg-gradient-to-br from-zinc-200 to-zinc-500 bg-clip-text text-center text-transparent">
-            {bestFor}
-          </span> */}
-        </div>
-        <div className="space-y-2 md:space-y-4 py-3 md:py-9">
+          </span>}
+        </div>}
+        {benefits && <div className="space-y-2 md:space-y-4 py-3 md:py-9">
           {benefits.map((benefit, index) => (
             <Benefit key={index} {...benefit} />
           ))}
-        </div>
-        {/* <Button className="w-full" variant={tier === "Pro" ? "default" : "ghost"}>
-          {CTA}
-        </Button> */}
+        </div>}
       </Card>
     </motion.div>
   );
